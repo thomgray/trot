07572e090996e01908ea5e8b93d5fa03e4757836
Thom Gray <thom.gray@bbc.co.uk>
2018-01-07 18:15:32 +0000

    removed sample out and modified run

--- lib/trot/run.rb
+++ lib/trot/run.rb
@@ -3,7 +3,7 @@ module Trot
     def run
       Trot.build
       target = $config.build_target
-      args = ARGV.drop 1
+      args = ARGV.drop(1).join(' ')
       system("#{target} #{args}")
     end
   end
